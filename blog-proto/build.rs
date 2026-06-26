6f512faec0828593f24d5e55417b0ab98ba1ffa6
Danila Volkov <danila.volkov@noxlovette.com>
2026-06-26 14:06:05 +0300

    prost ready

--- blog-proto/build.rs
+++ blog-proto/build.rs
@@ -1,5 +1,13 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("proto/blog.proto")?;
+use std::{env, path::PathBuf};
+
+fn main() -> anyhow::Result<()> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+
     println!("cargo:rerun-if-changed=proto/blog.proto");
+
+    tonic_prost_build::configure()
+        .file_descriptor_set_path(out_dir.join("blog_descriptor.bin"))
+        .compile_protos(&["proto/blog.proto"], &["proto"])?;
+
     Ok(())
 }
--- blog-proto/src/lib.rs
+++ blog-proto/src/lib.rs
@@ -1,1 +1,3 @@
-
+pub mod blog {
+    include!(concat!(env!("OUT_DIR"), "/blog.rs"));
+}
